5e1bae075ac2ead58b2966ffd19fd27b5db6b3f3
manuelapa <manuelr.aparicio@hotmail.com>
2023-12-13 15:36:23 -0600

    added place order button

---
+++ src/ConfirmOrder.js
@@ -0,0 +1,11 @@
+
+
+const ConfirmOrder = () => {
+  return (
+    <div className="confirm-order-container">
+        <h4>Your order has been placed!</h4>
+    </div>
+  )
+}
+
+export default ConfirmOrder
--- src/InfoOfCheckout.js
+++ src/InfoOfCheckout.js
@@ -1,7 +1,12 @@
 import CardForSummary from "./CardForSummary";
+import ConfirmOrder from "./ConfirmOrder";
+import { useState } from "react";
+
 
 const InfoOfCheckout = ({ itemsInCart }) => {
     
+ const [placeOrder, setPlaceOrder] = useState(false);
+
  const flattenedArray = itemsInCart.flat();
  let totalBill = 0;
  
@@ -24,20 +29,38 @@ for(let i = 0; i < flattenedArray.length; i++){
 
 let shippingTotal = Math.floor(totalBill*0.1) / 100;
 
+const placeOrderConfirm = () => {
+  if(itemsInCart.length > 0){
+    setPlaceOrder(!placeOrder);
+  }else{
+    alert("No items on cart.")
+  }
+}
+
   return (
     <div className="summary-container">
         <div className="summary-list">
             <h3>SUMMARY</h3>
-            {newArrayWithRepeat.map(item => <CardForSummary item={item} key={item.id} />)}
+            {itemsInCart.length > 0 ? 
+            newArrayWithRepeat.map(item => <CardForSummary item={item} key={item.id} />) :
+            <p>No items on cart.</p>
+            }
+            
         </div>
-        <div className="total-bill-container">
+        {placeOrder ?  
+          <ConfirmOrder /> :
+          <div className="total-bill-container">
             <p>Total:${totalBill.toFixed(2)}</p>
             <p>Shipping: ${shippingTotal}</p>
             <p><strong>Grand Total: ${(totalBill+shippingTotal).toFixed(2)}</strong></p>
-            <button className="place-order-button">PLACE ORDER</button>
+            <button onClick={placeOrderConfirm} className="place-order-button">PLACE ORDER</button>
         </div>
+        }
     </div>
   )
 }
 
 export default InfoOfCheckout
+
+
+/**{newArrayWithRepeat.map(item => <CardForSummary item={item} key={item.id} />)} */
